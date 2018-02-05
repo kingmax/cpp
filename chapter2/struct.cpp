c7a13a07fbedf7f76892eeb21783ed6d00fea9a1
iJasonLee <kingmax_res@163.com;184327932@qq.com>
2018-02-05 17:10:00 +0800

    struct Sales_data

---
+++ chapter2/struct.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	struct Sales_data
+	{
+		std::string bookNo;
+		unsigned units_sold = 0;
+		double revenue = 0.0;
+	};
+	
+	Sales_data accum, trans, *salesptr;
+	
+	struct Foo{};
+	
+	
+	return 0;
+}
