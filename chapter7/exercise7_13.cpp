781fd87d49832a2eb9964170ea495bd380e9b563
iJasonLee <kingmax_res@163.com;184327932@qq.com>
2018-02-21 18:32:51 +0800

    exercise7_13

--- chapter7/exercise7_13.cpp
+++ chapter7/exercise7_13.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+#include "Book_sales.h"
 using namespace std;
 
 int main()
 {
+	//0-201-78345-x 3 20.00
+	cout << "Enter:isbn sold price" << endl;
+	Book_sales total(cin);
+	if(!total.isbn().empty())
+	{
+		//print(cout, total);
+		Book_sales trans;
+		while(read(cin, trans))
+		{
+			if(trans.isbn() == total.isbn())
+				total.combine(trans);
+			else
+			{
+				print(cout, total) << endl;
+				total = trans;
+			}
+		}
+		print(cout, total) << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+		return -1;
+	}
 	
 	return 0;
 }
