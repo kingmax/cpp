76e09d6cefca3d7d3d1dfb92a4eb4a350ada519d
iJasonLee <kingmax_res@163.com;184327932@qq.com>
2018-05-02 21:20:01 +0800

    chapter12/review

---
+++ chapter12/review/StrBlob.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef vector<string>::size_type size_type;
+
+class StrBlob
+{
+public:
+	//typedef vector<string>::size_type size_type;
+	StrBlob();
+	StrBlob(initializer_list<string> il);
+	size_type size() const {return data->size();}
+	bool empty() const {return data->empty();}
+	void push_back(const string &s){data->push_back(s);}
+	void pop_back();
+	string& front();
+	const string& front() const;
+	string& back();
+	const string& back() const;
+private:
+	shared_ptr<vector<string>> data;
+	void check(size_type i, const string &msg) const;
+};
+
+StrBlob::StrBlob() : data(make_shared<vector<string>>()){}
+StrBlob::StrBlob(initializer_list<string> il) : data(make_shared<vector<string>>(il)){}
+
+void StrBlob::check(size_type i, const string &msg) const
+{
+	if(i >= size())
+		throw out_of_range(msg);
+}
+
+string& StrBlob::front()
+{
+	check(0, "front on empty StrBlob");
+	return data->front();
+}
+const string& StrBlob::front() const
+{
+	check(0, "front on empty StrBlob");
+	return data->front();
+}
+string& StrBlob::back()
+{
+	check(0, "back on empty StrBlob");
+	return data->back();
+}
+const string& StrBlob::back() const
+{
+	check(0, "back on empty StrBlob");
+	return data->back();
+}
+void StrBlob::pop_back()
+{
+	check(0, "pop_back on empty StrBlob");
+	data->pop_back();
+}
+
+
+int main()
+{
+	StrBlob s({"hello"});
+	if(!s.empty())
+		cout << s.front() << endl;
+	
+	return 0;
+}
