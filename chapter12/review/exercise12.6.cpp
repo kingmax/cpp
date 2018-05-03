e4c4ed341bb652ff0dfab4d60025f2db61e1dd6b
iJasonLee <kingmax_res@163.com;184327932@qq.com>
2018-05-03 08:21:55 +0800

    exercise12.6

--- chapter12/review/exercise12.6.cpp
+++ chapter12/review/exercise12.6.cpp
@@ -3,8 +3,34 @@
 #include <memory>
 using namespace std;
 
+vector<int> *get_vi()
+{
+	vector<int> *vi = new vector<int>();
+	return vi;
+}
+
+void put_vi(vector<int> &vi)
+{
+	int x;
+	while(cin >> x)
+		vi.push_back(x);
+}
+
+void print_vi(vector<int> &vi)
+{
+	for(int x : vi)
+		cout << x << ' ';
+	cout << endl;
+}
+
 int main()
 {
+	vector<int> *vi = get_vi();
+	put_vi(*vi);
+	print_vi(*vi);
+	
+	delete vi;
+	vi = nullptr;
 	
 	return 0;
 }
