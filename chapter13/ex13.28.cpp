78c64ccfc772c0c357baae63803cc781fcba27a6
Jason <kingmax_res@163.com>
2018-07-27 10:57:30 +0800

    ex13.28

--- chapter13/ex13.28.cpp
+++ chapter13/ex13.28.cpp
@@ -2,18 +2,87 @@
 #include <iostream>
 using namespace std;
 
+//Reference Type?
 class TreeNode
 {
 private:
 	string value;
-	int count;
+	int* refcount;
 	TreeNode* left;
 	TreeNode* right;
 	
 public:
-	TreeNode() : value(""), count(0), left(nullptr), right(nullptr) {}
+	TreeNode() : value(""), refcount(new int(1)), left(nullptr), right(nullptr) {}
 	
-	TreeNode(const TreeNode& p) : value(p.value), count(p.count), left(p.left), right(p.right) {}
+	TreeNode(const TreeNode& p) : value(p.value), refcount(p.refcount), left(p.left), right(p.right) { ++*refcount; }
 	
+	TreeNode& operator=(const TreeNode&);
 	
+	~TreeNode();
 };
+
+TreeNode& TreeNode::operator=(const TreeNode& rhs)
+{
+	++*rhs.refcount;
+	if(--*refcount == 0)
+	{
+		delete left;
+		delete right;
+		delete refcount;
+	}
+	
+	left = rhs.left;
+	right = rhs.right;
+	value = rhs.value;
+	refcount = rhs.refcount;
+	
+	return *this;
+}
+
+TreeNode::~TreeNode()
+{
+	if(--*refcount == 0)
+	{
+		delete left;
+		delete right;
+		delete refcount;
+	}
+}
+
+//Value Type?
+class BinStrTree
+{
+private:
+	TreeNode *root;
+	
+public:
+	BinStrTree() : root(new TreeNode()){ cout << "default constructor\n"; }
+	BinStrTree(const BinStrTree& p) : root(new TreeNode(*p.root)){ cout << "copy constructor\n"; }
+	BinStrTree& operator=(const BinStrTree&);
+	~BinStrTree();
+};
+
+BinStrTree::~BinStrTree()
+{
+	delete root;
+}
+
+BinStrTree& BinStrTree::operator=(const BinStrTree& rhs)
+{
+	cout << "operator = \n";
+	TreeNode *_root = new TreeNode(*rhs.root);
+	delete root;
+	root = _root;
+	return *this;
+}
+
+int main()
+{
+	BinStrTree bst;
+	BinStrTree bt2(bst);
+	BinStrTree bt3 = bst; //copy constructor same as bt3(bst)!
+	
+	bt3 = bt2; //call operator=
+	
+	return 0;
+}
