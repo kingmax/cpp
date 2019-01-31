9e07296a16e1c6b2ae19d8af3b9080d2a217c54d
kingmax <kingmax_res@163.com>
2019-01-31 23:43:59 +0800

    + OOP2 main

---
+++ ch15oop/OOP2_Basic/main.cpp
@@ -0,0 +1,26 @@
+#include "Quote.h"
+#include "Bulk_quote.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+ostream &print_total(ostream &os, const Quote &quote, size_t n)
+{
+	double total = quote.net_price(n);
+	os << "ISBN: " << quote.isbn()
+		<< " # sold: " << n
+		<< " total due: " << total
+		<< endl;
+	return os;
+}
+
+int main()
+{
+	Quote base("0-201-82470-1", 50);
+	print_total(cout, base, 10);
+
+	Bulk_Quote derived("0-201-82470-2", 50, 5, 0.19);
+	print_total(cout, derived, 10);
+
+	return 0;
+}
