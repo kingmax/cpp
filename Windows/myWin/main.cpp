886fe603e47c8336f937a31e77747cc94a47f9c9
kingmax <kingmax_res@163.com>
2019-03-08 00:21:26 +0800

    + myWin (my first manually create c++ win32 base Window)

--- Windows/myWin/main.cpp
+++ Windows/myWin/main.cpp
@@ -1,7 +1,13 @@
 #include <Windows.h>
 #include <windowsx.h>
 
+#include "main.h"
+
 #define WIN32_LEAN_AND_MEAN
+
+const LPCWSTR className = L"myWindow";
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -31,11 +37,89 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	win.cbClsExtra = 0;
 	win.cbWndExtra = 0;
 	win.hInstance = hInstance;
-	win.hIcon = LoadIcon(NULL, IDI_ASTERISK);
+	//win.hIcon = LoadIcon(NULL, IDI_ASTERISK);
+	win.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
 	win.hCursor = LoadCursor(NULL, IDC_CROSS);
 	win.hbrBackground = (HBRUSH)GetStockObject(DKGRAY_BRUSH);
 	win.lpszMenuName = NULL;
-	win.lpszClassName = L"myWindow";
+	win.lpszClassName = className;
+	//win.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	win.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
 
-	return 0;
+	if (!RegisterClassEx(&win))
+	{
+		return 0;
+	}
+
+	/*
+	CreateWindowExW(
+    _In_ DWORD dwExStyle,
+    _In_opt_ LPCWSTR lpClassName,
+    _In_opt_ LPCWSTR lpWindowName,
+    _In_ DWORD dwStyle,
+    _In_ int X,
+    _In_ int Y,
+    _In_ int nWidth,
+    _In_ int nHeight,
+    _In_opt_ HWND hWndParent,
+    _In_opt_ HMENU hMenu,
+    _In_opt_ HINSTANCE hInstance,
+    _In_opt_ LPVOID lpParam);
+#ifdef UNICODE
+#define CreateWindowEx  CreateWindowExW
+	*/
+	
+	HWND hwnd;
+	hwnd = CreateWindowEx(
+		NULL, className, className,
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT, /*0, 0,*/ 640, 480,
+		NULL, NULL, hInstance, NULL
+	);
+
+	if (!hwnd)
+	{
+		return FALSE;
+	}
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+		{
+			
+		}
+		break;
+	case WM_COMMAND:
+		{
+			
+		}
+		break;
+	case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+			EndPaint(hwnd, &ps);
+		}
+		break;
+	case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+		}
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wparam, lparam);
+	}
 }
