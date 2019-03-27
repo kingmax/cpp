db34066c4074933fcd49bb6e1a5ca705110e0799
kingmax <kingmax_res@163.com>
2019-03-27 23:54:50 +0800

    * ch2_fibonacci

--- EssentialCPP/ch2_fibonacci.cpp
+++ EssentialCPP/ch2_fibonacci.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 int fibonacci_element(unsigned n)
@@ -43,6 +44,53 @@ bool fibonacci_element(int pos, int &result)
 	return true;
 }
 
+bool print_sequence(int pos)
+{
+	if(pos <= 0 || pos > 1024)
+	{
+		cerr << "invalid position: " << pos
+			 << " -- cannot handle request!\n";
+		return false;
+	}
+	
+	cout << "The Fibonacci Sequence for " << pos
+		 << " positions: \n\t";
+	
+	// 无论pos是多少都会输出1 1, 除了pos==1
+	// 因为有default, 且没有被break!
+	switch(pos)
+	{
+		default:
+		case 2:
+			cout << "1 ";
+			// No break here, so go down..
+		case 1:
+			cout << "1 ";
+			break;
+	}
+	
+	int elem;
+	int n2 = 1, n1 = 1;
+	for(int i = 3; i <= pos; ++i)
+	{
+		elem = n2 + n1;
+		n2 = n1;
+		n1 = elem;
+		cout << elem << (!(i % 10) ? "\n\t" : " ");
+	}
+	cout << endl;
+	
+	return true;
+}
+
+void print_msg(ostream &os, const string &msg)
+{
+	if(msg.empty())
+		return;
+	
+	os << msg;
+}
+
 int main()
 {
 	int pos;
@@ -58,6 +106,11 @@ int main()
 	else
 		cout << "Sorry, Could not calculate element #"
 			<< pos << endl;
+			
+	cout << endl;
+	print_sequence(pos);
+	
+	print_msg(cout, "hello, function");
 	
 	return 0;
 }
