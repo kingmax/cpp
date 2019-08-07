02fc987aa30b79c45c751a62d819aee59f1067d2
kingmax <kingmax_res@163.com>
2019-08-08 07:59:17 +0800

    get_set_env ok

--- get_set_env/get_set_env/main.cpp
+++ get_set_env/get_set_env/main.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include <cstdlib>
 #include <iostream>
 using namespace std;
@@ -17,6 +18,26 @@ int setenv(const char *name, const char *value, int overwrite)
 	return _putenv_s(name, value);
 }
 
+// https://stackoverflow.com/questions/5246046/how-to-add-environment-variable-in-c
+bool SetPermanentEnvironmentVariable(LPCTSTR value, LPCTSTR data)
+{
+	HKEY hKey;
+	LPCTSTR keyPath = TEXT("System\\CurrentControlSet\\Control\\Session Manager\\Environment");
+	LSTATUS lOpenStatus = RegOpenKeyEx(HKEY_LOCAL_MACHINE, keyPath, 0, KEY_ALL_ACCESS, &hKey);
+	if (lOpenStatus == ERROR_SUCCESS)
+	{
+		LSTATUS lSetStatus = RegSetValueEx(hKey, value, 0, REG_SZ, (LPBYTE)data, strlen(data) + 1);
+		RegCloseKey(hKey);
+
+		if (lSetStatus == ERROR_SUCCESS)
+		{
+			SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)"Environment", SMTO_BLOCK, 100, NULL);
+			return true;
+		}
+	}
+
+	return false;
+}
 
 int main()
 {
@@ -28,7 +49,36 @@ int main()
 		cout << env << " = " << val << endl;
 	}
 
-	cout << setenv("mytest", "TST", 0) << endl; 
+	//cout << setenv("mytest", "TST", 0) << endl;
+	LPCTSTR envVar = TEXT("MY_ENV");
+	LPCTSTR envVal = TEXT("MY_VAL");
+
+	//HKEY hKey;
+	//LPCTSTR envKey = TEXT("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment");
+	//if (ERROR_SUCCESS != RegOpenKeyEx(HKEY_LOCAL_MACHINE, envKey, 0, KEY_ALL_ACCESS, &hKey));
+	//{
+	//	cerr << "访问注册表出错！\n请使用管理员帐户执行本程序！" << endl;
+	//	return -1;
+	//}
+
+
+	//LSTATUS lSetStatus = RegSetValueEx(hKey, envVar, 0, REG_SZ, (LPBYTE)envVal, strlen(envVal) + 1);
+	//RegCloseKey(hKey);
+
+	//if (lSetStatus == ERROR_SUCCESS)
+	//{
+	//	SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)"Environment", SMTO_BLOCK, 100, NULL);
+	//}
+
+	if (SetPermanentEnvironmentVariable(envVar, envVal))
+	{
+		cout << "set env MY_ENV=MY_VAL ok" << endl;
+	}
+	else
+	{
+		cout << "set env MY_ENV=MY_VAL failed" << endl;
+	}
+	
 
 	system("pause");
 	return 0;
