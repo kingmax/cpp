b6249cb236ad7730c88cf0f5249209fb9dd697b5
kingmax <kingmax_res@163.com>
2019-08-30 00:59:21 +0800

    + strsafe api

---
+++ safe_length.c
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include <wchar.h>
+#include <strsafe.h>
+
+int wmain()
+{
+	wchar_t str[] = L"ZetCode";
+	size_t target_size = 0;
+	size_t size = sizeof(str);
+	
+	HRESULT r = StringCbLengthW(str, size, &target_size);
+	if(SUCCEEDED(r))
+	{
+		wprintf(L"The string has %lld bytes\n", target_size);
+	}
+	else
+	{
+		wprintf(L"StringCbLengthW() failed\n");
+		return 1;
+	}
+	
+	size = sizeof(str) / sizeof(wchar_t);
+	r = StringCchLengthW(str, size, &target_size);
+	if(SUCCEEDED(r))
+	{
+		wprintf(L"The string has %lld characters\n", target_size);
+	}
+	else
+	{
+		wprintf(L"StringCchLengthW() failed\n");
+		return 1;
+	}
+	
+	return 0;
+}
