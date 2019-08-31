1e416d3bd3db485d3ff602ba5443ae0faeec33ff
kingmax <kingmax_res@163.com>
2019-08-31 22:26:04 +0800

    + utctime, date_format, leapyear, arithmetic

--- localtime.c
+++ localtime.c
@@ -5,7 +5,9 @@ int wmain()
 {
 	SYSTEMTIME lt = {0};
 	GetLocalTime(&lt);
-	wprintf(L"The local time is: %02d:%02d:%02d\n", lt.wHour, lt.wMinute, lt.wSecond);
+	wprintf(L"The local time is: %04d/%02d/%02d %02d:%02d:%02d %ld\n", lt.wYear, lt.wMonth, lt.wDay, 
+	lt.wHour, lt.wMinute, lt.wSecond,
+	lt.wMilliseconds);
 	
 	return 0;
 }
